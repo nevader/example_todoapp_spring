ac02f92400d716adc97a1e5de3a2fbaf85286e5d
nevader <krzysiek.przybysz8@gmail.com>
2024-10-06 19:44:15 +0200

    added exception handling

---
+++ src/main/java/com/nevader/todo/exceptions/ErrorDetails.java
@@ -0,0 +1,24 @@
+package com.nevader.todo.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+
+public class ErrorDetails {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
