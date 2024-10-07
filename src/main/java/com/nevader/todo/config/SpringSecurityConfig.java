246ec0df1351cee58623f131dd752b54f046f469
Nevader <krzysiek.przybysz8@gmail.com>
2024-10-07 15:57:52 +0200

    simple spring security setup SpringSecurityConfig.java

--- src/main/java/com/nevader/todo/config/SpringSecurityConfig.java
+++ src/main/java/com/nevader/todo/config/SpringSecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
